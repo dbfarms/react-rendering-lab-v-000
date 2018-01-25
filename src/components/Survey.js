7969522dee188592095bfa7dfce093ab6113e1db
dbfarms <matt.hunger@gmail.com>
2018-01-25 12:47:07 +0000

    Done.

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -14,6 +14,13 @@ class Survey extends React.Component {
       rating: 0
     };
   }
+  
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      increasing: nextProps.rating > this.props.rating,
+      decreasing: nextProps.rating < this.props.rating
+    })
+  }
 
   increaseRating = () => {
     this.setState({ rating: this.state.rating + 1 });
